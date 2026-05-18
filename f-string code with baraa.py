3e09eea7773ebc50daa2b05f3549175d53712ca4
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-18 17:37:49 +0700

    Refactor variable assignments and print statements

--- f-string code with baraa.py
+++ f-string code with baraa.py
@@ -2,10 +2,11 @@
 name = "sam"
 age = 34
 is_student = False
-plus = 2+3
+
 # print hasil joining all variable, value, dan string yang akan ditambahkan
 print(f"my name is {name}, my age is {age} and my status student is {is_student}")
 
+plus = 2+3
 #print hasil pertambahan
 print(f"2+3={2+3}")
 #atau
