82893e30d034c3302123c456d5dfa447a2b07aee
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-18 18:09:46 +0700

    Add files via upload

---
+++ Transformation (Indexing and Slicing) code with baraa.py
@@ -0,0 +1,12 @@
+# Transformation : Data extration (Indexing and slicing)
+
+word = "Hello"
+
+# print huruf yang akan diprint dari kata "Hello" index 1 hasil = e
+print(word[1])
+# print huruf yang akan diprint dari kata "Hello" index -1 hasil = o
+print(word[-1])
+# print huruf yang akan diprint dari kata "Hello" index -5 hasil = H
+print(word[-5])
+# print huruf yang akan diprint dari kata "Hello" index -6 hasil = error
+print(word[-6])
