bf54d627d9f0755e0964b43e7aec5872a4e51b37
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-18 18:01:09 +0700

    Add files via upload

---
+++ Transformation code with baraa.py
@@ -0,0 +1,22 @@
+# Transformation
+
+Info = "Adam-24-USA"
+# print split Hasil : ['Adam', '24', 'USA']
+print(Info.split("-"))
+
+stamp = "2026-09-20 14:30"
+# print split Hasil : ['2026-09-20', '14:30']
+print(stamp.split(" "))
+
+stamp = "2026-09-20"
+# print split Hasil : ['2026', '09', '20']
+print(stamp.split("-"))
+
+Info = "1234,Max,USA,1970-10-05,M"
+# print split Hasil : ['1234', 'Max', 'USA', '1970-10-05', 'M']
+print(Info.split(","))
+
+# pengulangan kata atau tanda menggunakan kali untuk menghemat waktu dan mempermudah membaca code
+print("hahaha")
+# ketika menggunakan kali
+print("ha"*3)
