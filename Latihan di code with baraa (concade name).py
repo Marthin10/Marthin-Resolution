87fbb5859c2e687853bdde87e4c7fa951026eece
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-18 16:41:00 +0700

    Refactor name concatenation and add comments
    Refactor code to improve clarity and add comments.

--- Latihan di code with baraa (concade name).py
+++ Latihan di code with baraa (concade name).py
@@ -4,7 +4,9 @@ joined_name = first_name+" "+second_name
 
 print(joined_name)
 
+#Variable, nama folder dan file, dan concade name
 folder = "C/Computer/CodeWithBaraa"
 file = "/NamaFile.csv"
-
 final_name = folder+file
+#print Final Name
+print("final_name")
