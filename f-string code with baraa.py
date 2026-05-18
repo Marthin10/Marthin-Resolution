b8ea25a621e0c189f74ed8eae3b7a2fd503ce952
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-18 16:47:59 +0700

    Add files via upload

---
+++ f-string code with baraa.py
@@ -0,0 +1,7 @@
+# variable dan isi variable
+name = "sam"
+age = 34
+is_student = False
+# print hasil joining all variable, value, dan string yang akan ditambahkan
+print(
+    f"my name is {name}, my age is {age} and my status student is {is_student}")
