773a290faaee039f80f3ce6ed7032013ddcc9afc
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-18 17:37:02 +0700

    Enhance f-string code with addition functionality
    Added a calculation for the sum of 2 and 3 and printed the result.

--- f-string code with baraa.py
+++ f-string code with baraa.py
@@ -2,6 +2,12 @@
 name = "sam"
 age = 34
 is_student = False
+plus = 2+3
 # print hasil joining all variable, value, dan string yang akan ditambahkan
 print(
     f"my name is {name}, my age is {age} and my status student is {is_student}")
+
+#print hasil pertambahan
+print(f"2+3={2+3}")
+#atau
+print(f"2+3={plus}")
