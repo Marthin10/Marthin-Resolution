fbb2fe429f6dd53b2609e85ca4a1f828ddb05065
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-19 22:18:29 +0700

    Add files via upload

---
+++ Chapter 2 Challenge Code With Baraa.py
@@ -0,0 +1,10 @@
+text = "968-Maria, ( D@t@ Engineer );; 27 y  "
+
+text = text.strip()
+
+ni = text[0:3]
+name = text[4:9]
+role = text[13:26].replace("@", "a")
+age = text[-4:]
+
+print(f"Number Indetity {ni}, Name {name}, role : {role}, age : {age}ears")
