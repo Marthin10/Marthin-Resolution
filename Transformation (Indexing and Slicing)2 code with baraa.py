ce26731b11e204d851ed1d0af891b9fecb9a2081
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-19 13:47:46 +0700

    Add files via upload

---
+++ Transformation (Indexing and Slicing)2 code with baraa.py
@@ -0,0 +1,11 @@
+# idexes and slicing
+date = "2026-09-20"
+
+# Extract whole number and sign
+print(date[0:])
+# Extract the year
+print(date[0:4])
+# Extract the month
+print(date[5:7])
+# Extract the date
+print(date[8:10])
