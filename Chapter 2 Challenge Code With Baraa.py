aaf4ad9432f6a2724212b0f0c9abf017dd788b3b
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-19 22:28:47 +0700

    Update Chapter 2 Challenge Code With Baraa.py

--- Chapter 2 Challenge Code With Baraa.py
+++ Chapter 2 Challenge Code With Baraa.py
@@ -7,4 +7,4 @@ name = text[4:9]
 role = text[13:26].replace("@", "a")
 age = text[-4:]
 
-print(f"Number Indetity {ni}, Name {name}, role : {role}, age : {age}ears")
+print(f"Number Indetity : {ni}, Name {name}, role : {role}, age : {age}ears")
