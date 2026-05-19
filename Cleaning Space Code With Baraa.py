ed6d75f7b49412ec97d60008c9f3cf72bbdf2c63
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-19 20:34:51 +0700

    Add files via upload

---
+++ Cleaning Space Code With Baraa.py
@@ -0,0 +1,10 @@
+# cleaning space at the right, left and both side using method strip
+text = "   Engineering   "
+
+print(text)
+# Clean both side
+print(text.strip())
+# Clean right side
+print(text.rstrip())
+# Clean left side
+print(text.lstrip())
