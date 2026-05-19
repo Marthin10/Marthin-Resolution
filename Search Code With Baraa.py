28f7d2fa2b8671e32ba283c499c5e7a4e74bdcab
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-19 22:48:52 +0700

    Add files via upload

---
+++ Search Code With Baraa.py
@@ -0,0 +1,11 @@
+# Search
+number = "+49-176-12345"
+
+print(number.startswith("+49"))
+
+email = "Baraa@gmail.com"
+print(email.endswith("1gmail.com"))
+print("@" in email)
+
+url = "https://api.company.com/v1/data"
+print("/api" in url)
