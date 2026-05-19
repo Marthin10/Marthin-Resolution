f5aa4d197ba38d61656cca5181edd22c3ea387e2
Marthin Virgo Bahagia <Marthinvirgo.ixd@gmail.com>
2026-05-19 21:58:49 +0700

    Add files via upload

---
+++ Case Conversion Code With Baraa.py
@@ -0,0 +1,14 @@
+# Case Conversion
+text = "python PROGRAMMING"
+print(text.lower())
+print(text.upper())
+
+text1 = "Email"
+text2 = "emaIL"
+
+print(text1 == text2)
+
+text1 = "Email".lower().strip()
+text2 = "emaIL".lower()
+
+print(text1 == text2)
